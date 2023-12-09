893c8e2819fc86e9c2ac26a4e3f0b42ca1aaa15f
Ubuntu <ubuntu@ip-172-31-32-193.ap-northeast-2.compute.internal>
2023-12-09 03:42:21 +0000

    open and filesize

--- userprog/build/userprog/syscall.d
+++ userprog/build/userprog/syscall.d
@@ -9,4 +9,5 @@ userprog/syscall.o: ../../userprog/syscall.c \
  ../../include/threads/flags.h ../../include/intrinsic.h \
  ../../include/threads/mmu.h ../../include/threads/pte.h \
  ../../include/threads/vaddr.h ../../include/userprog/process.h \
- ../../include/filesys/filesys.h ../../include/filesys/off_t.h
+ ../../include/filesys/filesys.h ../../include/filesys/off_t.h \
+ ../../include/filesys/file.h
